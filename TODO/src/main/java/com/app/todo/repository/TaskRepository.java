5fbf08d2ed97700c3709eadf79a34b30c214cbc1
KavyaSuvarna <kavyasuvarna90@gmail.com>
2018-01-29 08:59:20 +0400

    Add files via upload

--- TODO/src/main/java/com/app/todo/repository/TaskRepository.java
+++ TODO/src/main/java/com/app/todo/repository/TaskRepository.java
@@ -1,1 +1,22 @@
+package com.app.todo.repository;
 
+import java.util.List;
+
+import javax.transaction.Transactional;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.app.todo.model.TaskEntity;
+
+@Transactional
+public interface TaskRepository extends JpaRepository<TaskEntity, Integer> {
+	
+	@Query("SELECT t FROM TaskEntity t WHERE t.iscomplete=TRUE")
+    public List<TaskEntity> findByIscomplete();
+	
+	@Query("SELECT t FROM TaskEntity t WHERE t.iscomplete=FALSE")
+    public List<TaskEntity> findByIscompleteFalse();
+	
+	
+}
