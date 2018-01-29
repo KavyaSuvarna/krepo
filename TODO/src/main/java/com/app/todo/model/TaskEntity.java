3c4ae3f0f59f1ea444447208c8c86912fee979d4
KavyaSuvarna <kavyasuvarna90@gmail.com>
2018-01-29 08:57:21 +0400

    Add files via upload

--- TODO/src/main/java/com/app/todo/model/TaskEntity.java
+++ TODO/src/main/java/com/app/todo/model/TaskEntity.java
@@ -1,1 +1,46 @@
+package com.app.todo.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "TASK")
+public class TaskEntity {
+	
+	@Id
+    @GeneratedValue
+    @Column(name="ID")
+	private int id;
+	
+	@Column(name="NAME")
+	private String taskName;
+	
+	@Column(name="ISCOMPLETE")
+	private Boolean iscomplete;
+	
+	public TaskEntity() {
+		
+	}
+	
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getTaskName() {
+		return taskName;
+	}
+	public void setTaskName(String taskName) {
+		this.taskName = taskName;
+	}
+	public Boolean getIscomplete() {
+		return iscomplete;
+	}
+	public void setIscomplete(Boolean iscomplete) {
+		this.iscomplete = iscomplete;
+	}
+}
