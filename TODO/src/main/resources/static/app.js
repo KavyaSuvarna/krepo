bfdaa2af839ef2055179275f32fe6835d00be1fa
KavyaSuvarna <kavyasuvarna90@gmail.com>
2018-01-29 09:04:21 +0400

    Add files via upload

--- TODO/src/main/resources/static/app.js
+++ TODO/src/main/resources/static/app.js
@@ -1,1 +1,67 @@
+'use strict';
 
+angular.module("todoApp", [])
+
+.controller('todoController', ['$scope', '$http', function($scope, $http) {
+	 $http({
+	      method : "GET",
+	      url : 'http://localhost:8080/todo'
+	  }).then(function mySuccess(response) {
+		 
+	  });
+	  
+	  $scope.addTask = function() {
+	    
+	    $http({
+	      method : "GET",
+	      url : 'http://localhost:8080/todo/addTask/'+$scope.newTask
+	      
+	  }).then(function mySuccess(response) {
+		  delete $scope.newTask;
+		  var json_response = @Html.Raw(Json.Encode(Model));
+		  alert(json_response);
+		  
+	  })};
+ 
+	 $scope.deleteTask = function(taskId){
+		 $http({
+		      method : "GET",
+		      url : 'http://localhost:8080/todo/deleteTask/'+taskId
+		  }).then(function mySuccess(response) {
+		      
+		  })};
+	 
+	 $scope.taskComplete = function(taskId){
+		 $http({
+		      method : "GET",
+		      url : 'http://localhost:8080/todo/completeTask/'+taskId
+		  }).then(function mySuccess(response) {
+		      
+		  })};
+	 
+	  $scope.pendingTasks = function(){
+		  $http({
+		      method : "GET",
+		      url : 'http://localhost:8080/todo/getPendingTasks'
+		  }).then(function mySuccess(response) {
+		      
+		  })};
+
+	  $scope.completedTasks = function(){
+		  $http({
+		      method : "GET",
+		      url : 'http://localhost:8080/todo/getCompletedTasks'
+		  }).then(function mySuccess(response) {
+		      
+		  })};
+	  
+	  $scope.allTasks = function(){
+		  $http({
+		      method : "GET",
+		      url : 'http://localhost:8080/todo'
+		  }).then(function mySuccess(response) {
+		      
+		  })};
+	  
+	  
+}]);
