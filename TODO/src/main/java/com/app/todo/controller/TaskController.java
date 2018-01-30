d78956c182b9af5599e952f77ab1e83aad36c457
KavyaSuvarna <kavyasuvarna90@gmail.com>
2018-01-30 16:37:55 +0400

    Add files via upload

--- TODO/src/main/java/com/app/todo/controller/TaskController.java
+++ TODO/src/main/java/com/app/todo/controller/TaskController.java
@@ -1,66 +1,52 @@
 package com.app.todo.controller;
 
-import java.util.List;
-import java.util.Map;
-
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
 
-import com.app.todo.model.TaskEntity;
+import com.app.todo.model.Response;
 import com.app.todo.service.TaskService;
 
-@Controller
+@RestController
 public class TaskController {
 	
 	@Autowired
 	private TaskService taskService;
 	
-	@RequestMapping(value ="/todo", method = RequestMethod.GET)
-	public String getTasks(Model model) {
-		List<TaskEntity> taskList = taskService.getAllTasks();
-	    model.addAttribute("taskList",taskList);
-	    return "index";
+	@RequestMapping(value ="/todo/getAll", method = RequestMethod.GET)
+	public Response getTasks() {
+		return new Response("taskList",taskService.getAllTasks());
 	  }
 
     @RequestMapping(value = "/todo/addTask/{taskName}", method = RequestMethod.GET)
-    public String addTask(@PathVariable("taskName") String taskName, Map<String, Object> model ) {
+    public Response addTask(@PathVariable("taskName") String taskName) {
         taskService.addTask(taskName);
-        List<TaskEntity> taskList = taskService.getAllTasks();
-	    model.put("taskList",taskList);
-	    return "index";
+        return new Response("taskList",taskService.getAllTasks());
     }
     
     @RequestMapping(value = "/todo/deleteTask/{taskId}", method = RequestMethod.GET)
-    public String deleteTask(@PathVariable("taskId") int taskId, Model model ) {
+    public Response deleteTask(@PathVariable("taskId") int taskId, Model model ) {
     	taskService.deleteTask(taskId);
-    	List<TaskEntity> taskList = taskService.getAllTasks();
-	    model.addAttribute("taskList",taskList);
-		return "index";
+    	return new Response("taskList",taskService.getAllTasks());
     	
     }
     
     @RequestMapping(value = "/todo/completeTask/{taskId}", method = RequestMethod.GET)
-    public String taskComplete(@PathVariable("taskId") int taskId, Model model ){
+    public void taskComplete(@PathVariable("taskId") int taskId, Model model ){
     	taskService.completeTask(taskId);
-    	return "index";
     }
     
 	@RequestMapping(value = "/todo/getPendingTasks", method = RequestMethod.GET)
-    public String getPendingTasks(Model model ){
-    	taskService.getPendingTasks();
-    	model.addAttribute("taskList",taskService.getPendingTasks());
-    	return "index";
+    public Response getPendingTasks(Model model ){
+		return new Response("taskList",taskService.getPendingTasks());
 	}
 	
 	@RequestMapping(value = "/todo/getCompletedTasks", method = RequestMethod.GET)
-    public String getCompletedTasks(Model model ){
-    	taskService.getCompletedTasks();
-    	model.addAttribute("taskList",taskService.getCompletedTasks());
-    	return "index";
+    public Response getCompletedTasks(Model model ){
+		return new Response("taskList",taskService.getCompletedTasks());
     	
     }
     
